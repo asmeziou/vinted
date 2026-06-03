e4d2d6da55c0e3045af214530cc2d57ff105545e
asmeziou <asmeziou@gmail.com>
2026-06-03 21:18:20 +0200

    correction bug

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,31 @@
 import js from "@eslint/js";
 import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
+  globalIgnores(["dist"]),
+  {
+    files: ["**/*.{js,jsx}"],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: { ecmaFeatures: { jsx: true } },
+    },
+    rules: {
+      "react/jsx-no-target-blank": "on",
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "react/no-unescaped-entities": 0, // <=== ICI
+      "react/prop-types": "off", // <=== ET LÀ
+      "react/no-unused-vars": true,
+    },
+  },
 ]);
